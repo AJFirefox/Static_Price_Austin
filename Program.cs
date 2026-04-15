20045df6cf7df00325ec10387f93870c33a67fb7
AJFirefox <AJFirefox08@gmail.com>
2026-04-15 08:30:32 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Static_Price_Austin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
